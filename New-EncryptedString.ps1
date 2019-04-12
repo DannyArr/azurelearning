22f08d90b287c7337c1435eba3f7f2e35e9329a0
DannyArr <16082818+DannyArr@users.noreply.github.com>
2019-04-12 19:20:15 +0100

    Set up code for creating SP

--- New-EncryptedString.ps1
+++ New-EncryptedString.ps1
@@ -1,14 +1,20 @@
 function New-EncryptedString {
     param(
-        [parameter(mandatory=$true)]
+        [parameter(mandatory=$true,ParameterSetName="plaintext")]
         [string]$String,
+
+        [parameter(mandatory=$true,ParameterSetName="securestring")]
+        [securestring]$SecureString,
 
         [string]$KeyPath = '/home/danny/keys/dansazure_aes.key'
     )
 
     $key = Get-Content $keyPath
 
-    $secureString = ConvertTo-SecureString $String -AsPlainText -Force
+    if($PSCmdlet.ParameterSetName -eq "plaintext") {
+        $SecureString = ConvertTo-SecureString $String -AsPlainText -Force
+    }    
+    
     $enc = ConvertFrom-SecureString -SecureString $secureString -Key $key
 
     return $enc
--- SetupServicePrincipal.ps1
+++ SetupServicePrincipal.ps1
@@ -1,4 +1,22 @@
-ConvertTo-SecureString "test" -AsPlainText -Force
+#https://docs.microsoft.com/en-us/powershell/azure/create-azure-service-principal-azureps?view=azps-1.7.0
 
-[Security.Cryptography.RNGCryptoServiceProvider]::Create()
+Connect-AzAccount
+
+$sp = New-AzADServicePrincipal -DisplayName ServicePrincipalName
+$enc = New-EncryptedString -SecureString $sp.Secret
+
+$spObject = [PSCustomObject]@{
+    Password = $enc
+    ServicePrincipal = $sp
+}
+
+$spObject | Export-Clixml ../../config/azurelearning/sp.xml
+
+
+<#  ##Removing an app and SP##
+
+    $appId = ""
+    Remove-AzADServicePrincipal -ApplicationId $appId -Force
+    Remove-AzADApplication -ApplicationId $appId -Force
+#>
 
